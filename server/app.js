cb103d0ddd1f5f11174b5d6eec7965fe7149e3ca
Djordje <mandic.djordje98@gmail.com>
2024-04-01 09:44:01 +0200

    moj komit sa git ignore

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
+
+const proba = 222;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
