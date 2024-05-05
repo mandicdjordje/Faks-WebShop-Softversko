1cbc2765f83b18ee4df46fea1d6426f3519b8dbe
Djordje Mandic <mandic.djordje98@gmail.com>
2024-05-05 16:22:15 +0200

    finalno

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,9 @@
 const CustomError = require(`../errors/index.js`);
 // const db = require('../models/index.js');
-const db = require('../models/index');
+const db = require("../models/index");
 
-const bcript = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcript = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
   const encriptedPassword = await db.korisnik.prototype.encryptPassword(
@@ -11,7 +11,7 @@ const register = async (req, res, next) => {
   );
 
   const isFirstAccount = await db.korisnik.findOne();
-  const role = isFirstAccount ? 'USER' : 'ADMIN_ROOT';
+  const role = isFirstAccount ? "USER" : "ADMIN_ROOT";
 
   const email = await db.korisnik.findOne({ where: { email: req.body.email } });
 
@@ -23,13 +23,13 @@ const register = async (req, res, next) => {
       email: req.body.email,
       role: role,
       isVerified: false,
-      token: '',
+      token: "",
     });
 
     res.status(200).json({ success: true });
   } else {
     res.status(400).json({ success: false });
-    throw new CustomError.BadRequestError('Email postoji');
+    throw new CustomError.BadRequestError("Email postoji");
   }
 };
 
@@ -37,19 +37,19 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    throw new CustomError.BadRequestError('Please provide email and password');
+    throw new CustomError.BadRequestError("Please provide email and password");
   }
   console.log(email);
   const user = await db.korisnik.findOne({ where: { email: email } });
   console.log(user);
   if (!user) {
-    throw new CustomError.UnauthenticatedError('User does not exist');
+    throw new CustomError.UnauthenticatedError("User does not exist");
   }
 
   const isPasswordCorrect = await bcript.compare(password, user.password);
 
   if (!isPasswordCorrect) {
-    throw new CustomError.UnauthenticatedError('Password is not correct');
+    throw new CustomError.UnauthenticatedError("Password is not correct");
   }
 
   const user_id = user.user_id;
